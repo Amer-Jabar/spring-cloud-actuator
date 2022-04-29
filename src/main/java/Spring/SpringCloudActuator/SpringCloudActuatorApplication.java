8640cc23d2ac505b2a620b2cb641081674f342c6
Amer-Jabar <amerjabar0000@gmail.com>
2022-04-29 15:22:43 +0300

    Added custom info

--- src/main/java/Spring/SpringCloudActuator/SpringCloudActuatorApplication.java
+++ src/main/java/Spring/SpringCloudActuator/SpringCloudActuatorApplication.java
@@ -1,11 +1,24 @@
 package Spring.SpringCloudActuator;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.info.InfoContributor;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class SpringCloudActuatorApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudActuatorApplication.class, args);
     }
+}
+
+
+@Component
+class Info implements InfoContributor {
+
+    @Override
+    public void contribute(org.springframework.boot.actuate.info.Info.Builder builder) {
+        builder.withDetail("additional info", "additional value");
+    }
+
 }
