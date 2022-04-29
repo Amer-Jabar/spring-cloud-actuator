9b19a8b6da2f7eb9ea2e37bfdcff88ed8615bbe7
Amer-Jabar <amerjabar0000@gmail.com>
2022-04-29 14:47:02 +0300

    Initialized repo for spring cloud actuator

---
+++ src/main/java/Spring/SpringCloudActuator/SpringCloudActuatorApplication.java
@@ -0,0 +1,11 @@
+package Spring.SpringCloudActuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudActuatorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudActuatorApplication.class, args);
+    }
+}
---
+++ src/test/java/Spring/SpringCloudActuator/SpringCloudActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package Spring.SpringCloudActuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudActuatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
